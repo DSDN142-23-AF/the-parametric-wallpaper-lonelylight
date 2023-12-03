63b8cb71fd0fac216019d03aa9b7a59620662bdf
ZihengWu <wuzihe@myvuw.ac.nz>
2023-12-04 08:57:05 +0900

    push a flower generater

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,9 +1,9 @@
 //your parameter variables go here!
 let rect_width  = 1;//pixel size(better not change it)
 let rect_height = 1;
-let r_data = Math.floor((Math.random()*100)+1);;//random choose a base rgb color from 1-100
-let g_data = Math.floor((Math.random()*100)+1);;
-let b_data = Math.floor((Math.random()*100)+1);;
+let r_data = Math.floor(Math.random()*255);;//random choose a base rgb color from 0-255
+let g_data = Math.floor(Math.random()*255);;
+let b_data = Math.floor(Math.random()*255);;
 let r_pointer = true;//rgb number gradient pointer(if true->raise;false->down)
 let g_pointer = true;
 let b_pointer = true;
@@ -32,6 +32,9 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   fill(r_data,g_data,b_data);
   rect(0,0,rect_width,rect_height);
   generate_color(0,0,r_data,g_data,b_data,true);//start the color fall
+  // default: 6 petals per ring max, base ring is deep green(like a sepal), with max 7 layers.
+  //stroke(1);//for debug
+  petal_generater(Math.random()*3+2,Math.random()*200,Math.random()*200,Math.random()*10+100,Math.random()*10+20,0,128,0,Math.random()*4+3)//start the flower
 }
 
 function randompointer(){
@@ -111,5 +114,32 @@ function rgbcheck(){
   }
   if(b_data<5){
     b_pointer = true;
+  }
+}
+
+//lets draw a flower, first we need to know circle math: get the xy by degree
+function get_location_x(x,r,a){
+  return x+r*Math.cos(a*Math.PI/180);
+}
+function get_location_y(y,r,a){
+  return y+r*Math.sin(a*Math.PI/180);
+}
+
+//then we make a random petal generater, we desire a flower have 1-6 petals, know we need to get the petal function.
+//so, a flower's petal is inward contracted, we need a funtion that generate a random number is smaller than given number.
+function random_small(number){
+  return Math.round(Math.random()*(number/2)+number/3);
+}
+//we need a petal drawing function by recurtion.
+function petal_generater(number,x,y,rd,r_ring,r,g,b,layer){//number:petal number, rd= radius from *last* petals, r_ring = how large the petal rings from *last* petal rings.
+  if(layer>0){
+    layer--;
+  let radius = random_small(rd);//this petal rings radius
+  let radius_ring = random_small(r_ring);//this petal rings radius
+  fill(r,g,b);
+  for(var i=1;i<number+1;i++){
+    circle(get_location_x(x,radius_ring,360/number*i),get_location_y(y,radius_ring,180/number*i),radius);//draw circle by angle
+  }
+  petal_generater(Math.round(Math.random()*5+1),x,y,radius,radius_ring,Math.random()*255,Math.random()*255,Math.random()*255,layer)//generate next flower ring
   }
 }
